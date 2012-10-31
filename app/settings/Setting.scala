ac5d1bceb4b894af3fdc168dc6d3534254c01c7a
daishi <daishi.hmr@gmail.com>
2012-11-01 07:05:07 +0900

    modify configs

--- app/settings/Setting.scala
+++ app/settings/Setting.scala
@@ -12,12 +12,12 @@ object Setting {
 
   def staticServerName = current.mode match {
     case Mode.Prod => "static.dev7.jp"
-    case _ => "localhost:8080"
+    case _ => "static.dev7.jp"
   }
 
   def dataDir = current.mode match {
     case Mode.Prod => "/home/webapp/doga.data"
-    case _ => "../uploaded"
+    case _ => "uploaded"
   }
 
   def userDir(implicit user: Option[User]) = user.map { u =>
@@ -26,7 +26,7 @@ object Setting {
 
   def cacheDir = current.mode match {
     case Mode.Prod => "/home/webapp/doga.cache"
-    case _ => "../cache"
+    case _ => "cache"
   }
 
 }
