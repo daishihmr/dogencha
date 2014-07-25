0efe3dade1748be275469a86a8bc8b523363b61a
daishihmr <daishi.hmr@gmail.com>
2014-07-25 18:08:45 +0900

    3.0.0

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
   val appName = "dogencha"
-  val appVersion = "2.0.2"
+  val appVersion = "3.0.0"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
